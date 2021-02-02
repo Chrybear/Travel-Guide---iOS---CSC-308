4dcb3713b6c82eb21a8b7d9e30b1c44e5ffe3e70
Chrybear <50463991+Chrybear@users.noreply.github.com>
2021-02-02 15:29:34 -0500

    Initial Push
    A simple iOS app to simulat a basic travel guide made for CSC 308 at EKU
    
    Testing out understanding of labels, images, and segments

--- Travel_Guide/ViewController.swift
+++ Travel_Guide/ViewController.swift
@@ -2,16 +2,62 @@
 //  ViewController.swift
 //  Travel_Guide
 //
-//  Created by user186035 on 2/2/21.
+//  Created by Charles Ryan Barrett on 2/2/21.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+    //Variable to track the current area. Default is Lexington
+    var curArea = "Lexington"
+    //Variable to track the current attraction of an area
+    var curAttr = "Keeneland"
 
+    //Label for name of current attraction
+    @IBOutlet weak var cur_info: UILabel!
+    
+    
+    //Image for the current attraction
+    @IBOutlet weak var cur_img: UIImageView!
+    
+    //Area menu changed
+    @IBAction func area_changed(_ sender: UISegmentedControl) {
+        //Area menu was changed, update the new area name, submenu, etc
+        curArea = sender.titleForSegment(at: sender.selectedSegmentIndex)!
+       
+        
+    }
+    
+    //Attraction menu changed
+    @IBAction func atrac_changed(_ sender: UISegmentedControl) {
+        //Area menu was changed, update the new area name, submenu, etc
+        curAttr = sender.titleForSegment(at: sender.selectedSegmentIndex)!
+        //update the info label
+        cur_info.text = curArea + ": " + curAttr
+        //Change image
+        changImg(Area: curAttr)
+        
+    }
+    
+    //This function is just used to change the image when a new attraction is selected
+    func changImg(Area area: String) {
+        switch area {
+        case "Keeneland":
+            cur_img.image = UIImage(named: "keen.jpg")
+        case "Arboretum":
+            cur_img.image = UIImage(named: "arb.jpg")
+        default:
+            return
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //default view
+        cur_img.image = UIImage(named: "keen")
+        cur_info.text = "Lexington: Keeneland"
     }
 
 
